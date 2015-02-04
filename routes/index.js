78fa8071fc236bc26323613df0cf93fb6bca5e9e
Victor Silva <victor@victor-laptop>
2015-02-04 00:31:41 -0300

    modificacion index.js en routes, enviando lista de ciudades al view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET que envia al index principal los datos de las ciudades en una lista de objetos city. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var cities = [
+  { name: 'Buenos Aires', temperature: '29' },
+  { name: 'Lima', temperature: '20' },
+  { name: 'santiago', temperature: '33' }
+];
+
+  res.render('index', {title: 'Bienvenido', cities: cities });
 });
+
 
 module.exports = router;
